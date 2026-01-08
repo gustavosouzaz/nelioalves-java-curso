24188f4d3c61fcc731b137ae3b2e259c47d896f9
gustavosouzaz <gustavodsouzaaa@gmail.com>
2026-01-08 17:28:25 -0300

    Entrada de dados de java part 2

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
 
-        String x;
-        int y;
-        double z;
-        char w;
-        x = sc.next();
-        y = sc.nextInt();
-        z = sc.nextDouble();
-        w = sc.next().charAt(0);
-        System.out.println("Você digitou: " + x);
-        System.out.println("Você digitou o número: " + y);
-        System.out.println("Você digitou o número decimal: " + z);
-        System.out.println("");
+        String x,y,z;
+
+        x = sc.nextLine();
+        y = sc.nextLine();
+        z = sc.nextLine();
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
 
 
 
