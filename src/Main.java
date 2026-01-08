5bf54f4e7ad60107eefa5f1478f200c3f0da9ed3
gustavosouzaz <gustavodsouzaaa@gmail.com>
2026-01-08 17:14:43 -0300

    Entrada de dados em Java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Olá mundo!");
+        Scanner sc = new Scanner(System.in);
+
+
+        String x;
+        int y;
+        double z;
+        char w;
+        x = sc.next();
+        y = sc.nextInt();
+        z = sc.nextDouble();
+        w = sc.next().charAt(0);
+        System.out.println("Você digitou: " + x);
+        System.out.println("Você digitou o número: " + y);
+        System.out.println("Você digitou o número decimal: " + z);
+        System.out.println("");
+
+
+
+        sc.close();
 
         }
     }
