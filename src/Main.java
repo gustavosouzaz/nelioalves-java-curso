e9b850ddc4d5297514a6bf35df85d22329ed5bb3
gustavosouzaz <84815168+gustavosouzaz@users.noreply.github.com>
2026-05-25 15:18:06 -0300

    feat: resolve exercício 03 de while - contagem de combustíveis em posto

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,30 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int x = sc.nextInt();
-        int y = sc.nextInt();
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
 
-        while (x != 0 && y != 0) {
-            if (x > 0 && y > 0) {
-                System.out.println("primeiro");
+        int tipo = sc.nextInt();
+
+        while (tipo != 4) {
+            if (tipo == 1) {
+                alcool = alcool + 1;
             }
-            else if (x < 0 && y > 0) {
-                System.out.println("segundo");
+            else if (tipo == 2) {
+                gasolina = gasolina + 1;
             }
-            else if (x < 0 && y < 0) {
-                System.out.println("terceiro");
+            else if (tipo == 3) {
+                diesel = diesel + 1;
             }
-            else {
-                System.out.println("quarto");
-            }
-            x = sc.nextInt();
-            y = sc.nextInt();
+
+            tipo = sc.nextInt();
         }
+
+        System.out.println("MUITO OBRIGADO");
+        System.out.println("Alcool: " + alcool);
+        System.out.println("Gasolina: " + gasolina);
+        System.out.println("Diesel: " + diesel);
+
     }
     }
