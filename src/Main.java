72d68715efc2eb7948a088ddcc1ce973b98664e3
gustavosouzaz <84815168+gustavosouzaz@users.noreply.github.com>
2026-05-25 17:22:55 -0300

    feat: resolve exercício URI 1067 - listagem de ímpares com for loop

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
         Scanner sc = new Scanner(System.in);
 
-        int alcool = 0;
-        int gasolina = 0;
-        int diesel = 0;
+        int x = sc.nextInt();
 
-        int tipo = sc.nextInt();
-
-        while (tipo != 4) {
-            if (tipo == 1) {
-                alcool = alcool + 1;
+        for (int i=1; i<=x; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
             }
-            else if (tipo == 2) {
-                gasolina = gasolina + 1;
-            }
-            else if (tipo == 3) {
-                diesel = diesel + 1;
-            }
-
-            tipo = sc.nextInt();
         }
-
-        System.out.println("MUITO OBRIGADO");
-        System.out.println("Alcool: " + alcool);
-        System.out.println("Gasolina: " + gasolina);
-        System.out.println("Diesel: " + diesel);
 
     }
     }
