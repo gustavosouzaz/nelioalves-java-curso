43707e94d830d312142100be251094b11a8cee98
gustavosouzaz <84815168+gustavosouzaz@users.noreply.github.com>
2026-05-25 15:15:14 -0300

    feat: resolve exercício 01 de while - validação de senha

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Locale.setDefault(Locale.US);
+
         Scanner sc = new Scanner(System.in);
 
-        double salario = sc.nextDouble();
+        int senha = sc.nextInt();
 
-        double imposto;
-        if (salario <= 2000.0) {
-            imposto = 0.0;
-        }
-        else if (salario <= 3000.0) {
-            imposto = (salario - 2000.0) * 0.08;
-        }
-        else if (salario <= 4500.0) {
-            imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
-        }
-        else {
-            imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+        while (senha != 2002) {
+            System.out.println("Senha Invalida");
+            senha = sc.nextInt();
         }
 
-        if (imposto == 0.0) {
-            System.out.println("Isento");
-        }
-        else {
-            System.out.printf("R$ %.2f%n", imposto);
-        }
-
+        System.out.println("Acesso Permitido");
 
     }
     }
