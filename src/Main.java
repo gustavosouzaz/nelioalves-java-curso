530ab5c17e675145ed0b8b0235f680bc9570fcac
gustavosouzaz <84815168+gustavosouzaz@users.noreply.github.com>
2026-05-26 00:52:27 -0300

    feat: exercício 2 introdução POO - Correção dos exercícios de fixação (assunto: classes, atributos e métodos

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import entidades.Employee;
 import entidades.Product;
 import entidades.Rectangle;
 import entidades.Triangle;
@@ -11,15 +12,24 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Rectangle rect = new Rectangle();
+        Employee emp = new Employee();
 
-        System.out.println("Enter rectangle width and height:");
-        rect.width = sc.nextDouble();
-        rect.height = sc.nextDouble();
+        System.out.print("Name: ");
+        emp.name = sc.nextLine();
+        System.out.print("Gross salary: ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        emp.tax = sc.nextDouble();
 
-        System.out.printf("AREA = %.2f%n", rect.area());
-        System.out.printf("PERIMETER = %.2f%n", rect.perimeter());
-        System.out.printf("DIAGONAL = %.2f%n", rect.diagonal());
+        System.out.println();
+        System.out.println("Employee: " + emp);
+        System.out.println();
+        System.out.print("Which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        emp.increaseSalary(percentage);
+
+        System.out.println();
+        System.out.println("Updated data: " + emp);
 
     }
     }
