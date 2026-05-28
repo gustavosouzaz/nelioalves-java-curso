12ab084fb1bb9fea39198c61a031b0e453e3c922
gustavosouzaz <84815168+gustavosouzaz@users.noreply.github.com>
2026-05-28 13:01:51 -0300

    feat: adicionar exercício de vetores para cálculo de média de alturas

--- src/Main.java
+++ src/Main.java
@@ -8,41 +8,21 @@ public class Main {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Account account;
 
-        System.out.print("Enter account number: ");
-        int number = sc.nextInt();
-        System.out.print("Enter account holder: ");
-        sc.nextLine();
-        String holder = sc.nextLine();
-        System.out.print("Is there an initial deposit (y/n)? ");
-        char response = sc.next().charAt(0);
-        if (response == 'y') {
-            System.out.print("Enter initial deposit value: ");
-            double initialDeposit = sc.nextDouble();
-            account = new Account(number,holder,initialDeposit);
-         }else {
-            account = new Account(holder, number);
+        int n = sc.nextInt();
+        double[] vect = new double[n];
+
+        for(int i=0; i<n; i++) {
+            vect[i] = sc.nextDouble();
         }
 
-        System.out.println();
-        System.out.println("Account data:");
-        System.out.println(account);
+        double sum = 0.0;
+        for(int i=0; i<n; i++ ) {
+            sum += vect[i];
+        }
+        double avg = sum / n;
 
-        System.out.println();
-        System.out.print("Enter a deposit value: ");
-        double depositValue = sc.nextDouble();
-        account.deposit(depositValue);
-        System.out.println("Update account data: ");
-        System.out.println(account);
-
-        System.out.println();
-        System.out.print("Enter a withdraw value: ");
-        double withdrawValue = sc.nextDouble();
-        account.withdraw(withdrawValue);
-        System.out.println("Update account data: ");
-        System.out.println(account);
-
+        System.out.printf("Average Height: %.2f%n", avg);
 
 
     }
