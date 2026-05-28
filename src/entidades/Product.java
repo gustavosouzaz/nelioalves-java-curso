b65f333b2522bffcaa8c39b7072a06d36bfbb822
gustavosouzaz <84815168+gustavosouzaz@users.noreply.github.com>
2026-05-28 09:51:15 -0300

    feat: aula de sobrecaraga, exemplo problema feito

--- src/entidades/Product.java
+++ src/entidades/Product.java
@@ -8,8 +8,15 @@ public class Product {
 
    public Product(String name, double price, int quantity) {
           this.name = name;
+          this.price = price;
+          this.quantity = quantity;
 
    }
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+
+    }
 
    public double totalValueInStock() {
        return price * quantity;
