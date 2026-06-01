6e5ea15cb9298651deb3db438b0bf4f630f1fc10
gustavosouzaz <84815168+gustavosouzaz@users.noreply.github.com>
2026-06-01 12:36:08 -0300

    feat: exercício vetores - armazena N produtos e calcula preço médio

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,23 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        double[] vect = new double[n];
+       int n = sc.nextInt();
+       Aparelhos[] vect = new Aparelhos[n];
 
-        for(int i=0; i<n; i++) {
-            vect[i] = sc.nextDouble();
-        }
+       for(int i=0; i<n; i++) {
+           sc.nextLine();
+           String name = sc.nextLine();
+           double price  = sc.nextDouble();
+           vect[i] = new Aparelhos(name, price);
+       }
 
-        double sum = 0.0;
-        for(int i=0; i<n; i++ ) {
-            sum += vect[i];
-        }
-        double avg = sum / n;
+       double sum = 0.0;
+       for(int i=0; i<n; i++) {
+           sum += vect[i].getPrice();
+       }
+       double avg = sum / n;
 
-        System.out.printf("Average Height: %.2f%n", avg);
-
+        System.out.printf("AVERAGE PRICE = %.2f%n", avg);
 
     }
 }
